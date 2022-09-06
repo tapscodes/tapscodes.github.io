8b87c236e8d48479cc4814a88fb141ba5e64c70c
tapscodes <44383662+tapscodes@users.noreply.github.com>
2022-09-06 05:06:13 -0600

    (hopefully) fixed paths for GH pages

--- next.config.js
+++ next.config.js
@@ -6,8 +6,8 @@ const nextConfig = {
     loader: "akamai",
     path: "/",
   },
-  basePath: "/tapscodes.github.io",
-  assetPrefix: "/tapscodes.github.io",
+  basePath: "",
+  assetPrefix: "/",
 };
 
 module.exports = nextConfig;
