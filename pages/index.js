db89e0dc9147c4b5c893e5ecf528eeda35f75de2
tapscodes <44383662+tapscodes@users.noreply.github.com>
2022-09-07 15:00:11 -0600

    updated marquee text

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
       <Image src="/Images/prof_pic.jpg" width={128} height={128}/>
       <h3>Used Tools and Languages</h3>
       <Marquee speed="50" pauseOnHover="true" direction="right" gradient={true} gradientColor={[0,255,255]} gradientWidth={20}>
-        Java | C# | HTML | CSS | Javascript | React | C++ | Git | NextJS
+        Java | C# | HTML | CSS | Javascript | React | C++ | Git | NextJS | Swift | SQL | Cloudflare | JavaFX | Visual Studio 2022 | Visual Studio Code
       </Marquee>
     </div>
   )
