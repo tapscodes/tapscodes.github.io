94548632ab1f76224cb6d14577a23e547d532d1f
tapscodes <44383662+tapscodes@users.noreply.github.com>
2022-09-07 13:19:45 -0600

    added icon to navbar

--- xtra/components/Navbar.js
+++ xtra/components/Navbar.js
@@ -1,19 +1,19 @@
 import Link from "next/link";
+import Image from "next/image";
 
 const Navbar = () => {
     //resume link pulls from the 'public' folder.
     return ( 
         <nav>
-            <h1>Navbar</h1>
-            <Link href="/">Home</Link>
-            <Link href="/projects">Projects</Link>
-            <Link href="/about">About</Link>
-            <Link href="/PDFs/Resume.pdf">My Resume</Link>
+            <div className="navbar">
+                <Image src="Icons/android-chrome-192x192.png" width={64} height={64}/>
+                <Link href="/">Home</Link>
+                <Link href="/projects">Projects</Link>
+                <Link href="/about">About</Link>
+                <Link href="/PDFs/Resume.pdf">My Resume</Link>
+            </div>
         </nav>
      );
 }
  
 export default Navbar;
-<nav>
-    <h1>Navbar</h1>
-</nav>
