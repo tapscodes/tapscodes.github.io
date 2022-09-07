907582f3138fc4df6b649d763a98365070d2c311
tapscodes <44383662+tapscodes@users.noreply.github.com>
2022-09-07 00:44:15 -0600

    Added a few projects to the project page

--- pages/projects.js
+++ pages/projects.js
@@ -1,7 +1,23 @@
+import Link from "next/link";
+
 const Projects = () => {
     return ( 
         <div>
-            <p>Insert Projects Here</p>
+            <ul>
+                <h1>Personal Projects</h1>
+                <h3><Link href="https://github.com/tapscodes/tapscodes.github.io">Next.js Portfolio Website</Link></h3>
+                <p>The website you are currently viewing was made entirely with React and Next.JS.</p>
+                <h3><Link href="https://github.com/tapscodes/WatchList">WatchList</Link></h3>
+                <p>
+                    Watchlist is a personal project built in java that stores your shows watched,<br />
+                    the current episode you're on, and your current watching status.
+                </p>
+                <h3><Link href="https://github.com/tapscodes/FirstSite">HTML Portfolio Website</Link></h3>
+                <p>
+                    I originally created my portfolio website using nothing but basic html, css, and javascript.<br/>
+                    It is still being hosted in github pages. If you want to look at it, <Link href="https://tapscodes.github.io/firstsite">click here</Link>.
+                </p>
+            </ul>
         </div>
      );
 }
