078d5883224b28b273da368a0764186e0c0436a4
tapscodes <44383662+tapscodes@users.noreply.github.com>
2022-09-07 14:59:07 -0600

    added marquee to main page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Marquee from 'react-fast-marquee';
 
 export default function Home() {
   return (
@@ -8,9 +9,11 @@ export default function Home() {
                 <title>Tristan's Website</title>
                 <meta name="description" content="Tristan Spatscheck's Personal Portfolio Website."/>
       </Head>
-      <h1>SITE BEING CONSTRUCTED</h1>
       <Image src="/Images/prof_pic.jpg" width={128} height={128}/>
-      <h3>Come back in 1-2 days please.</h3>
+      <h3>Used Tools and Languages</h3>
+      <Marquee speed="50" pauseOnHover="true" direction="right" gradient={true} gradientColor={[0,255,255]} gradientWidth={20}>
+        Java | C# | HTML | CSS | Javascript | React | C++ | Git | NextJS
+      </Marquee>
     </div>
   )
 }
