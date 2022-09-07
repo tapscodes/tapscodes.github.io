03dac1546a384126b852be207e66d62d09bff8c2
tapscodes <44383662+tapscodes@users.noreply.github.com>
2022-09-07 13:46:51 -0600

    added external links to navbar

--- xtra/components/Navbar.js
+++ xtra/components/Navbar.js
@@ -6,11 +6,20 @@ const Navbar = () => {
     return ( 
         <nav>
             <div className="navbar">
-                <Image src="Icons/android-chrome-192x192.png" width={64} height={64}/>
+                <Image className="logo" src="Icons/android-chrome-192x192.png" width={64} height={64}/>
                 <Link href="/">Home</Link>
                 <Link href="/projects">Projects</Link>
                 <Link href="/about">About</Link>
                 <Link href="/PDFs/Resume.pdf">My Resume</Link>
+                <div className="navpic">
+                    <Link href="https://github.com/tapscodes"><Image src="Images/github.png" width={48} height={48}/></Link>
+                </div>
+                <div className="navpic">
+                    <Link href="https://www.linkedin.com/in/taps/"><Image src="Images/linked_in.png" width={48} height={48}/></Link>
+                </div>
+                <div className="navpic">
+                    <Link href="mailto:business@tristan-alexander.com"><Image src="Images/email.png" width={48} height={48}/></Link>
+                </div>
             </div>
         </nav>
      );
