bc04273c05848a6ba9d67e4d9692111d9e60512a
tapscodes <44383662+tapscodes@users.noreply.github.com>
2022-09-07 20:02:10 -0600

    Set up main page

--- pages/index.js
+++ pages/index.js
@@ -3,16 +3,23 @@ import Image from 'next/image'
 import Marquee from 'react-fast-marquee';
 
 export default function Home() {
+  const skills = "Java | C# | HTML | CSS | Javascript | React | C++ | Git | NextJS | Swift | SQL | MIPS | Cloudflare | JavaFX | Visual Studio 2022 | Visual Studio Code";
+
+  const quote = "Tristan Pudell-Spatscheck";
+
   return (
     <div className = "home">
       <Head>
                 <title>Tristan's Website</title>
                 <meta name="description" content="Tristan Spatscheck's Personal Portfolio Website."/>
       </Head>
-      <Image src="/Images/prof_pic.jpg" width={128} height={128}/>
+      <div className="quote_section">
+        <Image className="main_pic" src="/Images/prof_pic.jpg" width={128} height={128}/>
+        <h1 className="quote_text">{quote}</h1>
+      </div>
       <h3>Used Tools and Languages</h3>
-      <Marquee speed="50" pauseOnHover="true" direction="right" gradient={true} gradientColor={[0,255,255]} gradientWidth={20}>
-        Java | C# | HTML | CSS | Javascript | React | C++ | Git | NextJS | Swift | SQL | MIPS | Cloudflare | JavaFX | Visual Studio 2022 | Visual Studio Code
+      <Marquee speed="50" direction="right" gradient={true} gradientColor={[0,255,255]} gradientWidth={20}>
+        {skills}
       </Marquee>
     </div>
   )
