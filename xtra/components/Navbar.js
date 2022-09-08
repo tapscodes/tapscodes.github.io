e9f91396860fa0cf17a6dd7b4857961eecf44a76
tapscodes <44383662+tapscodes@users.noreply.github.com>
2022-09-07 23:06:58 -0600

    Scaled Images To NavBar

--- xtra/components/Navbar.js
+++ xtra/components/Navbar.js
@@ -6,14 +6,14 @@ const Navbar = () => {
     return ( 
         <nav>
             <div className="navbar">
-                <Image src="Icons/android-chrome-192x192.png" width={64} height={64}/>
+                <Image src="Icons/android-chrome-192x192.png" width="70vw" height="70vw"/>
                 <Link href="/">Home</Link>
                 <Link href="/projects">Projects</Link>
                 <Link href="/about">About</Link>
                 <Link href="/PDFs/Resume.pdf">Resume</Link>
-                <Link href="https://github.com/tapscodes"><a><Image src="Images/github.png" width={48} height={48}/></a></Link>
-                <Link href="https://www.linkedin.com/in/taps/"><a><Image src="Images/linked_in.png" width={48} height={48}/></a></Link>
-                <Link href="mailto:business@tristan-alexander.com"><a><Image src="Images/email.png" width={48} height={48}/></a></Link>
+                <Link href="https://github.com/tapscodes"><a><Image src="Images/github.png" width="50vw" height="50vw"/></a></Link>
+                <Link href="https://www.linkedin.com/in/taps/"><a><Image src="Images/linked_in.png" width="50vw" height="50vw"/></a></Link>
+                <Link href="mailto:business@tristan-alexander.com"><a><Image src="Images/email.png" width="50vw" height="50vw"/></a></Link>
             </div>
         </nav>
      );
