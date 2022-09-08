b45d808684c4a253b3e417d8128725f16f66d4b3
tapscodes <44383662+tapscodes@users.noreply.github.com>
2022-09-07 23:10:19 -0600

    Made Navbar items anchor text

--- xtra/components/Navbar.js
+++ xtra/components/Navbar.js
@@ -7,10 +7,10 @@ const Navbar = () => {
         <nav>
             <div className="navbar">
                 <Image src="Icons/android-chrome-192x192.png" width="70vw" height="70vw"/>
-                <Link href="/">Home</Link>
-                <Link href="/projects">Projects</Link>
-                <Link href="/about">About</Link>
-                <Link href="/PDFs/Resume.pdf">Resume</Link>
+                <Link href="/"><a>Home</a></Link>
+                <Link href="/projects"><a>Projects</a></Link>
+                <Link href="/about"><a>About</a></Link>
+                <Link href="/PDFs/Resume.pdf"><a>Resume</a></Link>
                 <Link href="https://github.com/tapscodes"><a><Image src="Images/github.png" width="50vw" height="50vw"/></a></Link>
                 <Link href="https://www.linkedin.com/in/taps/"><a><Image src="Images/linked_in.png" width="50vw" height="50vw"/></a></Link>
                 <Link href="mailto:business@tristan-alexander.com"><a><Image src="Images/email.png" width="50vw" height="50vw"/></a></Link>
