6788f69ff5ac83848af949f8c1a2b5f6ab5fb4fa
tapscodes <44383662+tapscodes@users.noreply.github.com>
2022-09-07 23:46:00 -0600

    abstracted clickable images + added params comment

--- xtra/components/Navbar.js
+++ xtra/components/Navbar.js
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import Image from "next/image";
+import ImageLink from "../../xtra/components/ImageLink";
 import styles from "../../styles//components/Navbar.module.css";
 
 const Navbar = () => {
@@ -7,16 +7,16 @@ const Navbar = () => {
         <nav>
             <div className={styles.navbar}>
                 <div className={styles.icon}>
-                    <Link href="/"><a><Image src="Icons/android-chrome-192x192.png" width="60vw" height="60vw"/></a></Link>
+                    <ImageLink href="/" src="Icons/android-chrome-192x192.png" width="60vw" height="60vw"/>
                 </div>
                 <div className={styles.other}>
                     <Link href="/"><a>Home</a></Link>
                     <Link href="/projects"><a>Projects</a></Link>
                     <Link href="/about"><a>About</a></Link>
                     <Link href="/PDFs/Resume.pdf"><a>Resume</a></Link>
-                    <Link href="https://github.com/tapscodes"><a><Image src="Images/github.png" width="40vw" height="40vw"/></a></Link>
-                    <Link href="https://www.linkedin.com/in/taps/"><a><Image src="Images/linked_in.png" width="40vw" height="40vw"/></a></Link>
-                    <Link href="mailto:business@tristan-alexander.com"><a><Image src="Images/email.png" width="40vw" height="40vw"/></a></Link>
+                    <ImageLink href="https://github.com/tapscodes" src="Images/github.png" width="40vw" height="40vw"/>
+                    <ImageLink href="https://www.linkedin.com/in/taps/" src="Images/linked_in.png" width="40vw" height="40vw"/>
+                    <ImageLink href="mailto:business@tristan-alexander.com" src="Images/email.png" width="40vw" height="40vw"/>
                 </div>
             </div>
         </nav>
