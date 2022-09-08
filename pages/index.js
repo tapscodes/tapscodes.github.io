147ebd54568752a7d9f84762c1e4608f08f6edc0
tapscodes <44383662+tapscodes@users.noreply.github.com>
2022-09-07 20:19:43 -0600

    Moved home page specific CSS to its own CSS file

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Marquee from 'react-fast-marquee';
+import styles from '../styles/Home.module.css';
 
 export default function Home() {
   const skills = "Java | C# | HTML | CSS | Javascript | React | C++ | Git | NextJS | Swift | SQL | MIPS | Cloudflare | JavaFX | Visual Studio 2022 | Visual Studio Code";
@@ -13,9 +14,9 @@ export default function Home() {
                 <title>Tristan's Website</title>
                 <meta name="description" content="Tristan Spatscheck's Personal Portfolio Website."/>
       </Head>
-      <div className="quote_section">
-        <Image className="main_pic" src="/Images/prof_pic.jpg" width={128} height={128}/>
-        <h1 className="quote_text">{quote}</h1>
+      <div className={styles.box}>
+        <Image className={styles.pic} src="/Images/prof_pic.jpg" width={128} height={128}/>
+        <h1 className={styles.text}>{quote}</h1>
       </div>
       <h3>Used Tools and Languages</h3>
       <Marquee speed="50" direction="right" gradient={true} gradientColor={[0,255,255]} gradientWidth={20}>
