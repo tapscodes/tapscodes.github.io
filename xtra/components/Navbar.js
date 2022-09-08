1984514c6bd504d8517f00cdc63351cc22a7a2f8
tapscodes <44383662+tapscodes@users.noreply.github.com>
2022-09-07 23:19:16 -0600

    Kept icon on left and moved others right in navbar

--- xtra/components/Navbar.js
+++ xtra/components/Navbar.js
@@ -5,14 +5,18 @@ const Navbar = () => {
     return ( 
         <nav>
             <div className="navbar">
-                <Image src="Icons/android-chrome-192x192.png" width="70vw" height="70vw"/>
-                <Link href="/"><a>Home</a></Link>
-                <Link href="/projects"><a>Projects</a></Link>
-                <Link href="/about"><a>About</a></Link>
-                <Link href="/PDFs/Resume.pdf"><a>Resume</a></Link>
-                <Link href="https://github.com/tapscodes"><a><Image src="Images/github.png" width="50vw" height="50vw"/></a></Link>
-                <Link href="https://www.linkedin.com/in/taps/"><a><Image src="Images/linked_in.png" width="50vw" height="50vw"/></a></Link>
-                <Link href="mailto:business@tristan-alexander.com"><a><Image src="Images/email.png" width="50vw" height="50vw"/></a></Link>
+                <div className="navicon">
+                    <Image src="Icons/android-chrome-192x192.png" width="60vw" height="60vw"/>
+                </div>
+                <div className="navmain">
+                    <Link href="/"><a>Home</a></Link>
+                    <Link href="/projects"><a>Projects</a></Link>
+                    <Link href="/about"><a>About</a></Link>
+                    <Link href="/PDFs/Resume.pdf"><a>Resume</a></Link>
+                    <Link href="https://github.com/tapscodes"><a><Image src="Images/github.png" width="40vw" height="40vw"/></a></Link>
+                    <Link href="https://www.linkedin.com/in/taps/"><a><Image src="Images/linked_in.png" width="40vw" height="40vw"/></a></Link>
+                    <Link href="mailto:business@tristan-alexander.com"><a><Image src="Images/email.png" width="40vw" height="40vw"/></a></Link>
+                </div>
             </div>
         </nav>
      );
