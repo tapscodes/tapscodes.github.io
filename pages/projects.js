5323da50267ff96d86936de53e5488f6f6fdb173
tapscodes <44383662+tapscodes@users.noreply.github.com>
2022-09-15 01:20:24 -0600

    Updated Project Page

--- pages/projects.js
+++ pages/projects.js
@@ -11,20 +11,14 @@ const Projects = () => {
             </Head>
             <ul>
                 <h1>Coding Projects</h1>
-                <ImageDescriptionBox href="https://github.com/tapscodes" target="_blank" img_url="Images/pizza.jpg" 
+                <ImageDescriptionBox href="https://github.com/tapscodes/tapscodes.github.io" target="_blank" img_url="Images/site_view.png" 
                 header = "Next.js Portofolio Website" text="The website you are currently viewing was made entirely with React and Next.JS."/>
-                <h3><Link href="https://github.com/tapscodes/tapscodes.github.io">Next.js Portfolio Website</Link></h3>
-                <p>The website you are currently viewing was made entirely with React and Next.JS.</p>
-                <h3><Link href="https://github.com/tapscodes/WatchList">WatchList</Link></h3>
-                <p>
-                    Watchlist is a personal project built with javafx + java that stores your shows watched,<br />
-                    the current episode you're on, and your current watching status.
-                </p>
-                <h3><Link href="https://github.com/tapscodes/FirstSite">HTML Portfolio Website</Link></h3>
-                <p>
-                    I originally created my portfolio website using nothing but basic html, css, and javascript.<br/>
-                    It is still being hosted in github pages. If you want to look at it, <Link href="https://tapscodes.github.io/FirstSite">click here</Link>.
-                </p>
+                <ImageDescriptionBox href="https://github.com/tapscodes/WatchList" target="_blank" img_url="Images/watchtime.png" 
+                header = "WatchList" text="Watchlist is a personal project built with javafx + java that stores your shows watched, 
+                the current episode you're on, and your current watching status."/>
+                <ImageDescriptionBox href="https://github.com/tapscodes/FirstSite" target="_blank" img_url="Images/first_site.png" 
+                header = "HTML Portfolio Website" text="I originally created my portfolio website using nothing but basic html, css, and javascript.
+                It is still being hosted in github pages if you'd want to look at it."/>
             </ul>
         </div>
      );
--- xtra/components/ImageDescriptionBox.js
+++ xtra/components/ImageDescriptionBox.js
@@ -11,7 +11,7 @@ const ImageDescriptionBox = (props) => {
     return ( 
         <div className={styles.box}>
             <div className={styles.image} style={{backgroundImage:"url(" + props.img_url + ")"}}>
-                <a href={props.href}>You Should Never See This Text, It's Just Here To Make The Image A Link</a>
+                <a href={props.href} target={props.target}>You Should Never See This Text, It's Just Here To Make The Image A Link</a>
             </div>
             <div className={styles.textbox}>
                 <h1>{props.header}</h1>
