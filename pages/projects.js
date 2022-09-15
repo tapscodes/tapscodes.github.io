def97f9c983ca67490b11b1bcfdc38c1ecb07cc5
tapscodes <44383662+tapscodes@users.noreply.github.com>
2022-09-15 01:34:44 -0600

    Added more description to first project

--- pages/projects.js
+++ pages/projects.js
@@ -11,7 +11,8 @@ const Projects = () => {
             <ul>
                 <h1 style={{fontSize: "5vw"}}>Coding Projects</h1>
                 <ImageDescriptionBox href="https://github.com/tapscodes/tapscodes.github.io" target="_blank" img_url="Images/site_view.png" 
-                header = "Next.js Portofolio Website" text="The website you are currently viewing was made entirely with React and Next.JS."/>
+                header = "Next.js Portofolio Website" text="The website you are currently viewing was made entirely with React and Next.JS.
+                It is being hosted on GitHub through GitHub Actions. It was made without the use of any templates."/>
                 <ImageDescriptionBox href="https://github.com/tapscodes/WatchList" target="_blank" img_url="Images/watchtime.png" 
                 header = "WatchList" text="Watchlist is a personal project built with javafx + java that stores your shows watched, 
                 the current episode you're on, and your current watching status."/>
